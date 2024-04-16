b6cbbe6cbbb78760637733bb4009799d8f6e0354
Kevin To <kevin.to@gmail.com>
2024-04-15 21:47:22 -0400

    Changes notification date field type from string to number

--- src/types/notification.ts
+++ src/types/notification.ts
@@ -2,16 +2,17 @@ interface Notification {
 	id: string;
 	alert: string;
 	message: string;
-	date: string;
+	date: number;
 	isSent: boolean;
 	createdAt: number;
 	isDeleted: boolean;
+	type: string;
 }
 
 interface NotificationRequest {
 	alert: string;
 	message: string;
-	date: string;
+	date: number;
 }
 
 export { Notification, NotificationRequest };
