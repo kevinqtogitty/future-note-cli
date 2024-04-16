85fb1a739f0cfbb2695ef771005cc546a6478874
Kevin To <kevin.to@gmail.com>
2024-04-15 21:46:08 -0400

    Completes fetching all notifications logic

--- src/controllers/notification/fetch-all-notifications.ts
+++ src/controllers/notification/fetch-all-notifications.ts
@@ -1,13 +1,14 @@
 import { Context } from 'koa';
 import mongodbClient from '@mongodb/instance';
-import { Db } from 'mongodb';
+import { Collection, Db } from 'mongodb';
 import { createUniversalResponse } from '@src/helpers/create-universal-response';
 export async function fetchAllNotifications(ctx: Context) {
 	try {
-		const db: Db = await mongodbClient({ database: 'props' });
-		const collection = db.collection('notifications');
+		const db: Db = await mongodbClient({ database: 'prod' });
+		const collection: Collection<Notification> = db.collection('notifications');
+		const documents = await collection.find({ type: 'NOTIFICATION' }).toArray();
 
-		ctx.body = createUniversalResponse({ ctx, status: 200, data: 'GOODBYE WORLD' });
+		ctx.body = createUniversalResponse({ ctx, status: 200, data: documents });
 	} catch (error) {
 		throw error;
 	}
